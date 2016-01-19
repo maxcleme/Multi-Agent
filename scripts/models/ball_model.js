e4784cc168927f923e8b89fe0a4ba976a893fd72
Engelaere Antoine <monkeyd.antoine@live.fr>
2016-01-19 02:00:04 +0100

    correction sur le comportement d'une bille

--- scripts/models/ball_model.js
+++ scripts/models/ball_model.js
@@ -1,67 +1,75 @@
 /**
  * Created by Quentin on 14/01/2015.
  */
-var BallModel = function (ballOptions) {
+var BallModel = function(ballOptions) {
 
-  var that = this;
+    var that = this;
 
-  var directions = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]];
-
-  /** FISH **/
-  var Ball = this.ball = function () {
-    Agent.call(this, 'ball', 'red', ballOptions.shape);
-    this.direction = directions[Math.floor(Math.random() * directions.length)];
-  };
-  Ball.prototype.doIt = function () {
-
-    var nextPosition = that.environment.getPosition(this.position.x + this.direction[0], this.position.y + this.direction[1]);
-
-    if (nextPosition == undefined) {
-      do {
-        var x = this.direction[0];
-        var y = this.direction[1];
-        if (x == 0 || y == 0) {
-          this.direction = [this.direction[0]*(-1),this.direction[1]*(-1)];
-        }
-        else if (x == 1 && y== 1) {
-          this.direction = [1,-1]
-        }
-        else if (x == 1 && y== -1) {
-          this.direction = [-1,-1]
-        }
-        else if (x == -1 && y== -1) {
-          this.direction = [-1,1]
-        }
-        else if (x == -1 && y== 1) {
-          this.direction = [1,1]
-        }
-      }
-      while ((nextPosition = that.environment.getPosition(this.position.x + this.direction[0], this.position.y + this.direction[1])) == undefined);
-    }
-    else {
-      var cell = that.environment.getAgent(nextPosition.x, nextPosition.y);
-      if (cell != undefined && cell instanceof Ball) {
-        this.direction = [this.direction[0]*(-1),this.direction[1]*(-1)];
-      }
-    }
-
-    that.environment.moveAgent(this, nextPosition.x, nextPosition.y);
-  };
-
-  /** STATS **/
-  this.statObject = function () {
-
-    this.reset = function () {
+    var directions = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+        [1, 1],
+        [-1, -1],
+        [-1, 1],
+        [1, -1]
+    ];
+	
+    /** BALL **/
+    var Ball = this.ball = function() {
+        Agent.call(this, 'ball', 'red', ballOptions.shape);
+        this.direction = directions[Math.floor(Math.random() * directions.length)];
     };
 
-    this.reset();
 
-    this.statsFunction = function (agent) {
+	Ball.prototype.nextPosition = function() {
+		return that.environment.getPosition(this.position.x + this.direction[0], this.position.y + this.direction[1]);
+	}
+		
+    Ball.prototype.doIt = function() {
+        var nextP;
+        var nbProcessed = 0;
+		
+        while (nbProcessed < directions.length && (nextP = this.nextPosition()) == undefined) {
+            var x = this.direction[0];
+            var y = this.direction[1];
+            if (x == 0 || y == 0) {
+                this.direction = [this.direction[0] * (-1), this.direction[1] * (-1)];
+            } else if (x == 1 && y == 1) {
+                this.direction = [1, -1]
+            } else if (x == 1 && y == -1) {
+                this.direction = [-1, -1]
+            } else if (x == -1 && y == -1) {
+                this.direction = [-1, 1]
+            } else if (x == -1 && y == 1) {
+                this.direction = [1, 1]
+            }
+            nbProcessed++;
+        }
 
+        if (nextP !== undefined) {
+            var cell = that.environment.getAgent(nextP.x, nextP.y);
+            if (cell !== undefined && cell instanceof Ball) {
+                this.direction = [this.direction[0] * (-1), this.direction[1] * (-1)];
+            } else {
+                that.environment.moveAgent(this, nextP.x, nextP.y);
+            }
+        }
     };
 
-    this.callback = function () {
+    /** STATS **/
+    this.statObject = function() {
+
+        this.reset = function() {};
+
+        this.reset();
+
+        this.statsFunction = function(agent) {
+
+        };
+
+        this.callback = function() {};
     };
-  };
 
 };
